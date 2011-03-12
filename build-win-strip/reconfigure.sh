b03dbd850311c0117f9a581e08ed415cff6b0730
Bryan Cain <bryancain3@gmail.com>
2011-03-12 17:58:11 -0600

    removed GNU Autotools build system

--- build-win-strip/reconfigure.sh
+++
@@ -1,5 +0,0 @@
-if test "x$1" = "x"; then
-	../configure  --host=i586-mingw32msvc --prefix=/usr/i586-mingw32msvc/
-else
-	../configure  --host=i586-mingw32msvc --prefix=/usr/i586-mingw32msvc/ --enable-debug
-fi
